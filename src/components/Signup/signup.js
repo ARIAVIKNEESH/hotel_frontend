8530c8c0924f700746c7c34bb47013d9b65ec412
ARIA VIKNEESH U S <107090810+ARIAVIKNEESH@users.noreply.github.com>
2024-11-16 00:09:33 +0530

    signup.js

--- src/components/Signup/signup.js
+++ src/components/Signup/signup.js
@@ -22,7 +22,7 @@ const Signup = () => {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/auth/signup', {
+      const response = await fetch('https://hotel-backend-1-8plr.onrender.com/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
