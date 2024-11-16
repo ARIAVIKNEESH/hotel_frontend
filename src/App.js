ef384cb31d97d4529285f140ff89362065cee349
ARIA VIKNEESH U S <107090810+ARIAVIKNEESH@users.noreply.github.com>
2024-11-16 22:04:53 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar/navbar';
 import Home from './pages/Home/home';
@@ -17,23 +17,26 @@ import { Elements } from '@stripe/react-stripe-js';
 const stripePromise = loadStripe('pk_test_51QLT1nEoMppwEzq5oA0znkEB41YaZyJX24of8UslKOxDQTVWaLhgXIJjfvcpGDIhzFAVAldcdtiOAjeJjVRU9Fn200eOw191aq');
 
 function App() {
-  // Function to handle logout, remove token and user info, and navigate to login page
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsLoggedIn(!!token);
+  }, []);
+
+  const handleLogin = () => setIsLoggedIn(true);
   const handleLogout = () => {
     localStorage.removeItem('token');
-    localStorage.removeItem('userName'); // Optionally remove user info
-    window.location.href = '/'; // Redirect to login page after logout
+    setIsLoggedIn(false);
   };
-
-  // Check if token exists in localStorage
-  const token = localStorage.getItem('token');
 
   return (
     <Router>
-      {token && <Navbar onLogout={handleLogout} />}
+      {isLoggedIn && <Navbar onLogout={handleLogout} />}
       <Routes>
-        {!token ? (
+        {!isLoggedIn ? (
           <>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Login onLogin={handleLogin} />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="*" element={<Navigate to="/" />} />
           </>
@@ -45,6 +48,8 @@ function App() {
             <Route path="/reviews" element={<Reviews />} />
             <Route path="/comparehotels" element={<CompareHotels />} />
             <Route path="/room/:id" element={<RoomDetails />} />
+            
+            {/* Wrap BookingForm with Elements provider */}
             <Route 
               path="/bookingform" 
               element={
@@ -53,6 +58,7 @@ function App() {
                 </Elements>
               } 
             />
+
             <Route path="/my-bookings" element={<MyBookings />} />
             <Route path="*" element={<Navigate to="/home" />} />
           </>
